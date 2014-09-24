f68e4fa0ecf448c2db6654706594bc767a115657
Ng_Xanh88 <ngxanh88@gmail.com>
2014-09-24 18:37:04 +0200

    url with infoMessage

--- frond_end/src/main/java/de/txa/baylennao/HomeController.java
+++ frond_end/src/main/java/de/txa/baylennao/HomeController.java
@@ -1,14 +1,8 @@
 package de.txa.baylennao;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import javax.inject.Inject;
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,85 +31,23 @@ public class HomeController {
 //	private EventService eventService;
 	
 	private Authentication auth;
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model,
-			@RequestParam(value = "logoutSuccess", required = false) String logoutSuccess,
-			@RequestParam(value = "loginFalue", required = false) String loginFalue) {
+	public String home(Model model) {
 
 		if(istLogin()) {
-		    logger.info(auth.getName() + " ist in system!!");
-		    model.addAttribute("user", userService.findByEmail(auth.getName()));	
-		}
-		
-		if(loginFalue != null) {
-			model.addAttribute("loginFalue", "login falue");
-		}
-		if(logoutSuccess != null) {
-			model.addAttribute("infoMessage", "logoutSuccess");
+		    model.addAttribute("user", userService.findByEmail(auth.getName()));
 		}
 		return "index";
-	}
-	
-	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public String profile(Locale locale, Model model,
-			@RequestParam(value = "newRegister", required = false) String newRegister) {
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-
-		if (newRegister != null) {
-			model.addAttribute("msg", "You muss logout before new Register!");
-		}
-		
-		if(istLogin()) {
-		    logger.info(auth.getName() + " ist in system!!");
-		    model.addAttribute("user", userService.findByEmail(auth.getName()));
-	        
-			return "profile";
-		}
-		return "redirect:/login";
 	}
 	
 	@RequestMapping(value = "/profile/update", method = RequestMethod.POST)
 	public String updateUser(Model model, UserDTO userDTO, BindingResult result) {
 		userService.update(userDTO);
-		return "redirect:/";
-	}
-	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Locale locale, Model model,
-						@RequestParam(value = "error", required = false) String error,
-						@RequestParam(value = "logout", required = false) String logout,
-						@RequestParam(value = "registerSuceess", required = false) String registerSuceess) {
-
-		if (istLogin()) {
-
-		    /* The user is logged in :) */			
-			return "redirect:/";
-		    
-		}
-		
-		if (error != null) {
-			model.addAttribute("error", "Invalid username and password!");
-		}
- 
-		if (logout != null) {
-			model.addAttribute("msg", "You've been logged out successfully.");
-		}
-		
-		if (registerSuceess != null) {
-			model.addAttribute("msg", "Registration successfully, you can login now!");
-		}
-		
-		return "login";
+		return "redirect:/?infoMessage=profileUpdateSuccess";
 	}
 	
 	@RequestMapping(value="/profile/changepassword", method = RequestMethod.POST)
@@ -126,43 +57,30 @@ public class HomeController {
 			@RequestParam(value = "re_password", required = true) String rePassword){
 		
 		if(newPassword.equals(rePassword)){
-			System.out.println(oldPassword);
-			System.out.println(newPassword);
-			System.out.println(rePassword);
-			
 			if(istLogin()){
-				System.out.println("email from logged in User:  "+auth.getName());
 				if(userService.checkPassswordBeforeChange(oldPassword, auth.getName())){
 					userService.changePassword(newPassword, auth.getName());
-					return "redirect:/?changePasswordSuccess";
+					return "redirect:/?infoMessage=changePasswordSuccess";
 				}
 			}
 		}
-		return "redirect:/?changePasswordFailure";
+		return "redirect:/?infoMessage=changePasswordFailure";
 	}
 	
 	@RequestMapping(value = "/register/add", method = RequestMethod.POST)
 	public String addUser(Model model, @Valid UserDTOtoCreate userDTOtoCreate, 
-					@RequestParam(value = "rePassword", required = true) String rePassword,
-					BindingResult result) {
-		if (result.hasErrors()) {
-			for (FieldError fieldError : result.getFieldErrors()) {
-//				model.addAttribute(fieldError.getField(), CSS_ERROR_TAG);
-				logger.error(fieldError.getField());
-			}
-		} else {
-			final UserDTO userDTO = userService.findByEmail(userDTOtoCreate.getEmail());
-			
-			if (userDTO != null && userDTO.getEmail() != null) {
-				return "redirect:/register?userExist";
-			}
-			
-			if(StringUtils.pathEquals(userDTOtoCreate.getPassword(), rePassword)) {
-				userService.create(userDTOtoCreate);
-				return "redirect:/?registerSuceess";
-			}
+					@RequestParam(value = "rePassword", required = true) String rePassword) {
+		final UserDTO userDTO = userService.findByEmail(userDTOtoCreate.getEmail());
+		
+		if (userDTO != null && userDTO.getEmail() != null) {
+			return "redirect:/?infoMessage=userExist";
 		}
-		return "redirect:/register?errorRegister";
+		
+		if(StringUtils.pathEquals(userDTOtoCreate.getPassword(), rePassword)) {
+			userService.create(userDTOtoCreate);
+			return "redirect:/?infoMessage=registerSuceess";
+		}
+		return "redirect:/?infoMessage=errorRegister";
 	}
 	
 	private boolean istLogin() {
