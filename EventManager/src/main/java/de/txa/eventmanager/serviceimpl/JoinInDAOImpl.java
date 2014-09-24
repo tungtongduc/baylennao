e7a3830e9286cb643202362b62e35233ab6d4171
Ng_Xanh88 <ngxanh88@gmail.com>
2014-09-24 05:47:17 +0200

    fix Annotation inject

--- EventManager/src/main/java/de/txa/eventmanager/serviceimpl/JoinInDAOImpl.java
+++ EventManager/src/main/java/de/txa/eventmanager/serviceimpl/JoinInDAOImpl.java
@@ -1,8 +1,11 @@
 package de.txa.eventmanager.serviceimpl;
 
+import org.springframework.stereotype.Component;
+
 import de.txa.eventmanager.entity.JoinInEntity;
 import de.txa.eventmanager.service.JoinInDAO;
 
+@Component
 public class JoinInDAOImpl extends BaseDAOImpl<JoinInEntity> implements JoinInDAO{
 
 }
