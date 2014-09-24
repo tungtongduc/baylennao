379e7f3ebc2a74e9e71ad99d965e890730ba2243
Ng_Xanh88 <ngxanh88@gmail.com>
2014-09-25 01:26:58 +0200

    fix query string

--- EventManager/src/main/java/de/txa/eventmanager/serviceimpl/EventDAOImpl.java
+++ EventManager/src/main/java/de/txa/eventmanager/serviceimpl/EventDAOImpl.java
@@ -1,6 +1,5 @@
 package de.txa.eventmanager.serviceimpl;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,12 +15,14 @@ import de.txa.eventmanager.service.EventDAO;
 @Component
 public class EventDAOImpl extends BaseDAOImpl<EventEntity> implements EventDAO{
 
+	private static final int FIRST_INDEX = 0;
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<EventEntity> findByEventName(String eventName) {
 		Query q = em.createQuery("SELECT e FROM EventEntity e WHERE e.eventName=:eventName");
 		q.setParameter("eventName", eventName);
-		return checkNullEventList(q.getResultList());
+		return q.getResultList();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -29,7 +30,7 @@ public class EventDAOImpl extends BaseDAOImpl<EventEntity> implements EventDAO{
 	public List<EventEntity> findByUserEmail(String userEmail) {
 		Query q = em.createQuery("SELECT e FROM EventEntity e WHERE e.hostEmail=:userEmail");
 		q.setParameter("userEmail", userEmail);
-		return checkNullEventList(q.getResultList());
+		return q.getResultList();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -37,34 +38,24 @@ public class EventDAOImpl extends BaseDAOImpl<EventEntity> implements EventDAO{
 	public List<EventEntity> findByDate(Date date) {
 		Query q = em.createQuery("SELECT e FROM EventEntity e WHERE e.eventDate=:date");
 		q.setParameter("date", date);
-		return checkNullEventList(q.getResultList());
+		return q.getResultList();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public JoinInEntity findInvite(String userEmail, Long eventId) {
-		Query q = em.createQuery("SELECT jn FROM EventEntity e join e.members jn WHERE e.id=:eventId and jn.userEmail=:userEmail");
+		Query q = em.createQuery("SELECT jn FROM EventEntity e join e.invites jn WHERE e.id=:eventId and jn.userEmail=:userEmail");
 		q.setParameter("eventId", eventId);
 		q.setParameter("userEmail", userEmail);
 		final List<JoinInEntity> joinInEntities = q.getResultList();
-		if(CollectionUtils.isEmpty(joinInEntities)) {
-			return null;
-		}
-		return joinInEntities.get(0);
+		return !CollectionUtils.isEmpty(joinInEntities) ? joinInEntities.get(FIRST_INDEX) : null;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<EventEntity> getAllInvites(String userEmail) {
-		Query q = em.createQuery("SELECT e FROM EventEntity e join e.members jn WHERE jn.userEmail=:userEmail");
+		Query q = em.createQuery("SELECT e FROM EventEntity e join e.invites jn WHERE jn.userEmail=:userEmail");
 		q.setParameter("userEmail", userEmail);
-		return checkNullEventList(q.getResultList());
-	}
-	
-	private List<EventEntity> checkNullEventList( List<EventEntity> events){
-		if(CollectionUtils.isEmpty(events)) {
-			return new ArrayList<EventEntity>();
-		}
-		return events;
+		return q.getResultList();
 	}
 }
